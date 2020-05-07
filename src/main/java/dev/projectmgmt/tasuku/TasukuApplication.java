0f0451736d33c9c39d9efda761f819b5ec2af1ca
srinivasu619 <srinivasu.618@gmail.com>
2020-05-07 22:56:20 +0530

    1️⃣ Initial Project Setup

---
+++ src/main/java/dev/projectmgmt/tasuku/TasukuApplication.java
@@ -0,0 +1,13 @@
+package dev.projectmgmt.tasuku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasukuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasukuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/projectmgmt/tasuku/TasukuApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projectmgmt.tasuku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasukuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
